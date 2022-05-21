93b3de22948b1adac96a82582f2504c0b05b4d17
Vitali Baranov <vitalib.freeman@gmail.com>
2022-05-21 06:23:23 +0300

    short sample url

--- counter.py
+++ counter.py
@@ -1,2 +1,9 @@
-def shorten(url):
-    return ""
+import requests
+
+
+def shorten(token, url):
+    bitly_url = "https://api-ssl.bitly.com/v4/shorten"
+    headers = {'Authorization': f'Bearer {token}'}
+    response = requests.post(bitly_url, headers=headers, json={"long_url": url})
+    response.raise_for_status()
+    return response.json()
--- tests.py
+++ tests.py
@@ -6,19 +6,23 @@ from dotenv import load_dotenv
 
 
 class TestBitLy(unittest.TestCase):
+
+    def setUp(self):
+        load_dotenv()
+
     def test_can_obtain_short_link(self):
-        url = 'www.yandex.ru'
-        shortened_url = counter.shorten(url)
-        self.assertIn("bit.ly", shortened_url)
+        url = 'https://google.ru'
+        token = os.getenv("BITLY_TOKEN")
+        shortened_link = counter.shorten(token, url)['link']
+        self.assertIn("bit.ly", shortened_link)
+        print(shortened_link)
 
     def test_connect_to_bitly(self):
-        load_dotenv()
         token = os.getenv("BITLY_TOKEN")
         url = "https://api-ssl.bitly.com/v4/user"
         headers = {'Authorization': f'Bearer {token}'}
         response = requests.get(url, headers=headers)
         response.raise_for_status()
-        print(response.text)
         self.assertIn("login", response.json())
 
 
