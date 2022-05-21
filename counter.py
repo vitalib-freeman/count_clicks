38e4e9a035220f3518c08f3927faccf399f199d3
Vitali Baranov <vitalib.freeman@gmail.com>
2022-05-21 11:42:32 +0300

    shorten url from cli
    shorten url from cli

--- counter.py
+++ counter.py
@@ -1,9 +1,25 @@
 import requests
 
+BITLY_SHORTEN_API_URL = "https://api-ssl.bitly.com/v4/shorten"
 
-def shorten(token, url):
-    bitly_url = "https://api-ssl.bitly.com/v4/shorten"
-    headers = {'Authorization': f'Bearer {token}'}
-    response = requests.post(bitly_url, headers=headers, json={"long_url": url})
+BITLY_TOKEN = 'ab0c2990687fcacff260f38a9b98e9a9d847709d'
+
+
+def shorten_link(token, url):
+    response = requests.post(
+        BITLY_SHORTEN_API_URL,
+        headers={'Authorization': f'Bearer {token}'},
+        json={"long_url": url}
+    )
+
     response.raise_for_status()
     return response.json()
+
+
+def main():
+    url = input()
+    print(shorten_link(BITLY_TOKEN, url)['link'])
+
+
+if __name__ == '__main__':
+    main()
--- tests.py
+++ tests.py
@@ -1,5 +1,8 @@
+import io
 import os
 import unittest
+from unittest.mock import patch
+
 import counter
 import requests
 from dotenv import load_dotenv
@@ -13,7 +16,7 @@ class TestBitLy(unittest.TestCase):
     def test_can_obtain_short_link(self):
         url = 'https://google.ru'
         token = os.getenv("BITLY_TOKEN")
-        shortened_link = counter.shorten(token, url)['link']
+        shortened_link = counter.shorten_link(token, url)['link']
         self.assertIn("bit.ly", shortened_link)
         print(shortened_link)
 
@@ -24,6 +27,13 @@ class TestBitLy(unittest.TestCase):
         response = requests.get(url, headers=headers)
         response.raise_for_status()
         self.assertIn("login", response.json())
+
+    def test_get_shorten_link_for_url_from_stdin(self):
+        with patch('sys.stdin', io.StringIO('https://google.com\n')) as stdin, \
+                patch('sys.stdout', new_callable=io.StringIO) as stdout:
+            counter.main()
+            self.assertIn("bitly", stdout.getvalue())
+
 
 
 if __name__ == '__main__':
